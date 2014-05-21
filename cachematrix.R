216a425c99aefea9b299ecc773f3a3e9bfc04be0
EliasVakkuri <elias.vakkuri@gmail.com>
2014-05-21 21:02:34 +0300

    Koodi valmis
    Kommentit pitää vielä lisätä

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,50 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inv <- NULL
+        
+        setMat <- function (y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        getMat <- function () {
+                x
+        }
+        
+        setInv <- function(inverse) {
+                inv <<- inverse
+        }
+        
+        getInv <- function() {
+                inv
+        }
+        
+        list (setMat = setMat, getMat = getMat,
+              setInv = setInv, getInv = getInv)
+        
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getInv()
+        
+        if (!is.null(inv)) {
+                message("Getting cached inverse")
+                return(inv)
+        }
+        
+        a <- x$getMat()
+        
+        inv <- solve(a)
+        
+        x$setInv(inv)
+        
+        inv
 }
